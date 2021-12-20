d13ab29b0d96072988d14e42cdd464435a7677bb
nabin747 <81608352+nabin747@users.noreply.github.com>
2021-12-20 12:36:27 +0545

    data type: string

---
+++ string/article.md
@@ -0,0 +1,11 @@
+# Data types
+
+## String
+
+A string in javascript must be surrounded by quotes.
+
+```js
+let str = "Hello";
+let str1 = 'Single quotes';
+let str2 = `can embed another ${str}`;
+```
---
+++ string/style.js
@@ -0,0 +1,9 @@
+let name1 = "Nabin";
+// the expression is String
+alert(`hello ${1}`); //hello 1
+
+//the expression is "Name"
+alert(`hello ${"ramss"}`);
+
+// the expression is a variable, embed it vannale {} vitra variable pass garea  ma same retuen garna other wise sabi j xa thei print garxa or output dinxa
+alert(`Hello ${name1}`);
